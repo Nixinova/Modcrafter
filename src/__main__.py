d0a09a5caf396eb494a631c87ec04f4e7bfbef9c
Nixinova <Nixinova@users.noreply.github.com>
2021-01-27 09:46:55 +1300

    Initial progress

---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+"""Modfile"""
+
+from src.init import init
+
+init()
---
+++ src/files/modfile.py
@@ -0,0 +1,11 @@
+"""Returns Modfile contents"""
+
+def default():
+    """Default Modfile contents"""
+
+    return """
+        name: My Mod
+        author: Nixinova
+        version: 1.0
+        
+    """
